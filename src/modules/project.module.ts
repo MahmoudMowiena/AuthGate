3a030ca09ea2c28573005f75c1b52b0d0b753625
mahmoudmowiena <mowiena8@gmail.com>
2024-07-02 01:14:52 +0300

    Delete paypal

--- src/modules/project.module.ts
+++ src/modules/project.module.ts
@@ -3,12 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Project, projectSchema } from 'src/domain/entities/project.entity';
 import { ProjectService } from 'src/infrastructure/services/project.service';
 import { ProjectsController } from 'src/presentation/controllers/project.controller';
-import { projectModel } from 'src/presentation/dtos/project.model';
 import { TenantModule } from './tenant.module';
 import { UserModule } from './user.module';
-import { AuthModule } from './auth.module';
-import { UsersService } from 'src/infrastructure/services/users.service';
-import { userModel } from 'src/presentation/dtos/user.model';
 
 @Module({
   imports: [
