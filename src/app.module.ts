86d4a1c97f3852cb226889c298e2d28e251367d6
AhmedAdly <157370741+ahadlyy@users.noreply.github.com>
2024-07-03 22:33:03 +0300

    finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    //MongooseModule.forRoot('mongodb://localhost:27017'),
-    MongooseModule.forRoot('mongodb://mowiena8:zG8NfJYpJTkXtXK9@ac-flhrjff-shard-00-00.0e2el9i.mongodb.net:27017,ac-flhrjff-shard-00-01.0e2el9i.mongodb.net:27017,ac-flhrjff-shard-00-02.0e2el9i.mongodb.net:27017/?replicaSet=atlas-h2rep0-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=AuthGate'),
+    MongooseModule.forRoot('mongodb://localhost:27017/AuthGate'),
+    //MongooseModule.forRoot('mongodb://mowiena8:zG8NfJYpJTkXtXK9@ac-flhrjff-shard-00-00.0e2el9i.mongodb.net:27017,ac-flhrjff-shard-00-01.0e2el9i.mongodb.net:27017,ac-flhrjff-shard-00-02.0e2el9i.mongodb.net:27017/?replicaSet=atlas-h2rep0-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=AuthGate'),
     UserModule,
     ProjectsModule,
     TenantModule,
